b6d3471cc25020bb250d419b31064b54b39a2319
user <g.brahmadatta@gmail.com>
2019-03-29 17:08:47 +0530

    minore fixes

--- app/src/main/java/escapadetechnologies/com/alphabeticalrecyclerview/FirstActivity.java
+++ app/src/main/java/escapadetechnologies/com/alphabeticalrecyclerview/FirstActivity.java
@@ -31,7 +31,18 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(FirstActivity.this,FavouritesData.class));
+
+                /*Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = (DatePickerDialog) DatePickerDialog.newInstance(
+                        FirstActivity.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.setAutoHighlight(true);
+                dpd.show(getFragmentManager(), "Datepickerdialog");*/
             }
         });
     }
+
 }
